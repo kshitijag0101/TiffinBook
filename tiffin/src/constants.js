7e37123aa833d6048bc3af622b3cb05a9d71dcb4
Kshitij Agarwal <agarwal.kshitij01@gmail.com>
2024-01-03 09:06:30 +0530

    updated

--- tiffin/src/constants.js
+++ tiffin/src/constants.js
@@ -1,4 +1,4 @@
-export const API_DOMAIN = "https://tense-jade-snaps.cyclic.cloud/";
+export const API_DOMAIN = "https://tense-jade-snaps.cyclic.cloud";
 // export const API_DOMAIN = "http://localhost:5000";
 const BASE_USER_API = API_DOMAIN + "/user";
 const BASE_ADMIN_API = API_DOMAIN + "/admin";
